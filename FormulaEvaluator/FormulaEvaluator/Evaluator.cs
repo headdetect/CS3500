1b139c71948bcd942e57c1d67b2af903537c55ca
headdetect <headdetective2@gmail.com>
2015-09-06 22:59:40 -0600

    Finished FormulaEvaluator

--- FormulaEvaluator/FormulaEvaluator/Evaluator.cs
+++ FormulaEvaluator/FormulaEvaluator/Evaluator.cs
@@ -30,27 +30,138 @@ namespace FormulaEvaluator
             // Get all tokens then remap and strip all whitespace //
             var tokens = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)").Select(element => {
                 return Regex.Replace(element, "\\s+", "");
-            });
+            }).Where(element => element != "");
 
             var OperationStack = new Stack<OperationToken>();
-            var Numbers = new Stack<int>();
-
-            int result = 0;
+            var NumberStack = new Stack<int>();
 
             foreach (var token in tokens)
             {
                 int? possibleNumber = getNumberOrNothing(token);
+                var operation = new OperationToken
+                {
+                    Operation = token
+                };
+                if (!operation.IsValid && !possibleNumber.HasValue)
+                {
+                    // Is a variable //
+                    possibleNumber = variableLookup(token);
+                }
+
                 if (possibleNumber.HasValue)
                 {
                     // Is number //
+
+                    if (OperationStack.Count >= 1)
+                    {
+                        var topOperation = OperationStack.Peek();
+                        if (topOperation.IsDivision || topOperation.IsMultiplication)
+                        {
+                            if (NumberStack.Count < 1) throw new ArgumentException("Invalid Syntax");
+
+                            OperationStack.Pop(); // Passed our test, remove from stack //
+
+                            var number = NumberStack.Pop();
+
+                            NumberStack.Push(topOperation.Apply(number, possibleNumber.Value));
+
+                            continue;
+                        }
+                                               
+                    }
+
+                    NumberStack.Push(possibleNumber.Value);                    
                 }
                 else
                 {
-                    // Is Operation //
+                    if (operation.IsAddition || operation.IsSubtraction)
+                    {
+                        // If there's already a pending operation. //
+                        if (OperationStack.Count >= 1)
+                        {
+                            var topOperation = OperationStack.Peek();
+                            if (topOperation.IsAddition || topOperation.IsSubtraction)
+                            {
+                                if (NumberStack.Count < 2) throw new ArgumentException("Invalid Syntax");
+
+                                OperationStack.Pop();
+
+                                var number1 = NumberStack.Pop();
+                                var number2 = NumberStack.Pop();
+
+                                NumberStack.Push(topOperation.Apply(number2, number1));
+                            }
+                        }
+
+                        OperationStack.Push(operation);
+                    }
+                    else if (operation.IsMultiplication || operation.IsDivision || operation.IsOpenBrace)
+                        // Nothing complex, just push operation to stack //
+                        OperationStack.Push(operation);
+                    else if (operation.IsClosingBrace)
+                    {
+                        if (OperationStack.Count >= 1)
+                        {
+                            // If there's already a pending operation. //
+                            var topOperation = OperationStack.Peek();
+                            if (topOperation.IsAddition || topOperation.IsSubtraction)
+                            {
+                                if (NumberStack.Count < 2) throw new ArgumentException("Invalid Syntax");
+
+                                OperationStack.Pop();
+
+                                var number1 = NumberStack.Pop();
+                                var number2 = NumberStack.Pop();
+
+                                NumberStack.Push(topOperation.Apply(number2, number1));
+
+                            }
+                        }
+
+                        // Check for ending parenthesis //
+                        if (OperationStack.Count == 0 || !OperationStack.Pop().IsOpenBrace) throw new ArgumentException("Expecting '('. Invalid syntax.");
+                        
+                        // Operation size could have changed, we need to check again //
+                        if (OperationStack.Count >= 1)
+                        {
+                            var topOperation = OperationStack.Peek();
+                            if (topOperation.IsDivision || topOperation.IsMultiplication)
+                            {
+                                if (NumberStack.Count < 1) throw new ArgumentException("Invalid Syntax");
+
+                                OperationStack.Pop(); // Passed our test, remove from stack //
+
+                                var b = NumberStack.Pop();
+                                
+                                if (!possibleNumber.HasValue)
+                                    possibleNumber = NumberStack.Pop();
+
+                                NumberStack.Push(topOperation.Apply(possibleNumber.Value, b));
+                            }
+                        }
+                    
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid Syntax. Cannot handle given operation.");
+                    }
                 }
             }
 
-            return result;
+            if (OperationStack.Count >= 2)
+                throw new ArgumentException("Invalid Syntax. Cannot handle given operation.");
+
+            if (OperationStack.Count == 1)
+            {
+                var b = NumberStack.Pop();
+                var a = NumberStack.Pop();
+
+                return OperationStack.Pop().Apply(a, b);
+            }
+            else
+            {
+                return NumberStack.Pop();
+            }
         }
 
 
@@ -67,12 +178,91 @@ namespace FormulaEvaluator
         /// <summary>
         /// A tiny structure for handling operations. Not needed, but helps keep things clean and scalable.
         /// </summary>
-        private static struct OperationToken
+        internal struct OperationToken
         {
+
             /// <summary>
             /// The operation belonging to this struct
             /// </summary>
-            public string Operation {get; set;}
+            public string Operation {get; internal set;}
+
+            /// <summary>
+            /// Returns true if the current operation is multiplication
+            /// </summary>
+            public bool IsMultiplication
+            {
+                get
+                {
+                    return Operation == "*";
+                }
+            }
+
+            /// <summary>
+            /// Returns true if the current operation is division
+            /// </summary>
+            public bool IsDivision
+            {
+                get
+                {
+                    return Operation == "/" || Operation == "÷";
+                }
+            }
+
+            /// <summary>
+            /// Returns true if the current operation is addition
+            /// </summary>
+            public bool IsAddition
+            {
+                get
+                {
+                    return Operation == "+";
+                }
+            }
+
+
+            /// <summary>
+            /// Returns true if the current operation is addition
+            /// </summary>
+            public bool IsSubtraction
+            {
+                get
+                {
+                    return Operation == "-";
+                }
+            }
+
+            /// <summary>
+            /// If the operation is a opening parenthesis '('
+            /// </summary>
+            public bool IsOpenBrace
+            {
+                get
+                {
+                    return Operation == "(";
+                }
+            }
+
+            /// <summary>
+            /// If the operation is a closing parenthesis ')'
+            /// </summary>
+            public bool IsClosingBrace
+            {
+                get
+                {
+                    return Operation == ")";
+                }
+            }
+
+            /// <summary>
+            /// Returns true if provided operation/token can be handled as an operation
+            /// </summary>
+            public bool IsValid
+            {
+                get
+                {
+                    return IsAddition || IsSubtraction || IsMultiplication || IsDivision || IsOpenBrace || IsClosingBrace;
+                }
+            }
 
             /// <summary>
             /// Apply the operation to the specified integers.
@@ -82,7 +272,26 @@ namespace FormulaEvaluator
             /// <returns>The result of the application</returns>
             public int Apply(int a, int b)
             {
-                return 0;
+                if (IsMultiplication)
+                    return a * b;
+                if (IsAddition)
+                    return a + b;
+                if (IsSubtraction)
+                    return a - b;
+                if (IsDivision)
+                    return a / b;
+                if (IsOpenBrace || IsClosingBrace)
+                    throw new ArgumentException("Brace is not an acceptable operation");
+                throw new ArgumentException("No acceptable operation found");
+            }
+
+            /// <summary>
+            /// Returns string representation of this structure.
+            /// </summary>
+            /// <returns>String representation of this structure.</returns>
+            public override string ToString()
+            {
+                return string.Format("\"{0}\"", Operation);
             }
         }
 
--- FormulaEvaluator/PoopyWolframAlpha/Program.cs
+++ FormulaEvaluator/PoopyWolframAlpha/Program.cs
@@ -9,26 +9,149 @@ namespace PoopyWolframAlpha
 {
     class Program
     {
+        private const int
+            A = 5,
+            B = 10,
+            C = -3,
+            D = 1,
+            E = 0,
+            F = 100;
+
+        static Dictionary<string, int> DefaultVariableLookup = new Dictionary<string, int>() {
+            {"A", A},
+            {"B", B},
+            {"C", C},
+            {"D", D},
+            {"E", E},
+            {"F", F}
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("To denote a variable do <expression>;A=2;B=33;C=-2 etc...");
-            Console.WriteLine("Enter your expression:");
-            var expression = Console.ReadLine();
+            WriteLine("Press Enter to begin tests");
+            Console.ReadLine();
 
-            var tokens = expression.Split(';');
-            if (tokens.Length > 1)
+            // These tests all assume C# arithmic parser is correct. //
+
+            // Simple operations //
+
+            Test("3", 3);
+            Test("2 + 4", 2 + 4);
+            Test("5 * 3", 5 * 3);
+            Test("12 / 6", 12 / 6);
+            Test("4 - 2", 4 - 2);
+            Test("2 - 4", 2 - 4);
+            Test("6 / 1", 6 / 1);
+
+            // Simple operations with variables // 
+            Test("A", A);
+            Test("2 + A", 2 + A);
+            Test("5 * B", 5 * B);
+            Test("12 / C", 12 / C);
+            Test("4 - D", 4 - D);
+            Test("E - 2", E - 2);
+            Test("600 / F", 600 / F);
+
+            // Long operations with variables // 
+            Test("2 + A + A + 3 + 3", 2 + A + A + 3 + 3);
+            Test("5 * B * A * C * 2", 5 * B * A * C * 2);
+            Test("12 / C / 4", 12 / C / 4);
+            Test("E - 2 - 5 - 9 - A", E - 2 - 5 - 9 - A);
+            Test("600 / F / 2", 600 / F / 2);
+            
+            // Complex operations with variables & parens // 
+            Test("2 + A - A + 3 - 3", 2 + A - A + 3 - 3);
+            Test("5 * B / A * C / 2", 5 * B / A * C / 2);
+            Test("E + 2 - 5 / 9 * A", E + 2 - 5 / 9 * A);
+            Test("(A + B) - D * (F - C)", (A + B) - D * (F - C));
+            Test("(A - 4) + D / (F + C)", (A - 4) + D / (F + C));
+            Test("(A * 3) * (9 * 3) + D - E * (F - C) + (4 - 3) + A", (A * 3) * (9 * 3) + D - E * (F - C) + (4 - 3) + A);
+
+            // Gimme them errors //
+            Test("2 / 0", shouldFail: true); // Divide by 0 //
+            Test("G + 3", shouldFail: true); // Variable not found //
+            Test("2^2", shouldFail: true); // Not a valid operation //
+            Test("1 + ", shouldFail: true); // Not a valid syntax //
+            Test("1 + )", shouldFail: true); // Not a valid syntax //
+            Test("(3 + 4 + 5", shouldFail: true); // Not a valid syntax //
+            Test("C / E", shouldFail: true); // Divide by 0 //
+
+            WriteLine("To denote a variable do <expression>;A=2;B=33;C=-2 etc...");
+
+            while (true)
             {
-                // Has variables //
-                
+                WriteLine("Enter your expression (type Quit to exit):");
+                var expression = Console.ReadLine();
+
+                if (expression.ToLower() == "quit")
+                    return;
+
+                Test(expression);
             }
+        }
 
-            var result = Evaluator.Evaluate("1+1", (varb) =>
+        static void Test(string expression, int? shouldBe = null, bool shouldFail = false)
+        {
+            Dictionary<string, int> lookupDictionary = null;
+
+            try
             {
-                return 0;
-            });
+                var tokens = expression.Split(';');
+                if (tokens.Length > 1)
+                {
+                    // Has variables //
+                    expression = tokens[0];
+                    lookupDictionary = new Dictionary<string, int>(tokens.Length - 1);
 
-            Console.WriteLine(result);
-            Console.Read();
+                    for (int i = 1; i < tokens.Length; i++)
+                    {
+                        var token = tokens[i];
+                        var key = token.Split('=')[0];
+                        var value = int.Parse(token.Split('=')[1]);
+                        lookupDictionary.Add(key, value);
+                    }
+                }
+
+                lookupDictionary = lookupDictionary ?? DefaultVariableLookup;
+
+                var result = Evaluator.Evaluate(expression, (varb) =>
+                {
+                    return lookupDictionary[varb];
+                });
+
+                Write("(");
+                if (shouldBe.HasValue)
+                    if (shouldBe.Value == result)
+                        Write("Pass", ConsoleColor.Green);
+                    else
+                        Write("Fail", ConsoleColor.Red);
+                else Write("???", ConsoleColor.Red);
+                Write(") " + expression + " = " + result);
+                if (shouldBe.HasValue && shouldBe.Value != result)
+                    Write(" (expecting: " + shouldBe.Value + ")");
+                Write("\n");
+
+            }
+            catch (Exception e)
+            {
+                Write("(");
+                if (shouldFail) Write("Pass", ConsoleColor.Green);
+                else Write("Fail", ConsoleColor.Red);
+                WriteLine(") " + expression + " = " + e.Message);
+            }
+        }
+
+        private static void Write(string write, ConsoleColor color = ConsoleColor.White)
+        {
+            var pre = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(write);
+            Console.ForegroundColor = pre;
+        }
+
+        private static void WriteLine(string write, ConsoleColor color = ConsoleColor.White)
+        {
+            Write(write + "\n", color);
         }
     }
 }
