da56bdda562ffdd353192a4e85c6578aa94a070b
headdetect <headdetective2@gmail.com>
2015-11-24 16:20:02 -0700

    Adding a bunch of comments

--- PS7/AgCubio/ServerNetworkController/ServerNetwork.cs
+++ PS7/AgCubio/ServerNetworkController/ServerNetwork.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -8,6 +9,9 @@ using System.Threading.Tasks;
 
 namespace ServerNetworkController
 {
+    /// <summary>
+    /// Network class for the server implementation of AgCubio.
+    /// </summary>
     public class ServerNetwork
     {
         /// <summary>
@@ -21,10 +25,22 @@ namespace ServerNetworkController
         /// </summary>
         public static event Action<int> ClientLeft;
 
+
+        /// <summary>
+        /// A dicitonary of all the clients connected to the server. Using the UID as the key.
+        /// </summary>
         public Dictionary<int, TcpClient> Clients { get; }
 
+
+        /// <summary>
+        /// The TcpListener of the server. 
+        /// </summary>
         public TcpListener TcpListener { get; }
 
+        /// <summary>
+        /// Gets if the server is currently accepting connection. 
+        /// To set to false, close the connection by using <see cref="Stop()"/>
+        /// </summary>
         public bool Listening { get; private set; }
 
         public ServerNetwork()
@@ -39,13 +55,13 @@ namespace ServerNetworkController
         /// </summary>
         public void Listen()
         {
-            TcpListener.Start();
+            TcpListener.Start(); // Starts the listener
             Listening = true;
 
             while (Listening)
             {
-                var client = TcpListener.AcceptTcpClient();
-                ClientJoined?.Invoke(client);
+                var client = TcpListener.AcceptTcpClient(); // Accepts a new TcpClient from the listener //
+                ClientJoined?.Invoke(client); // Send an event that we've received a client.
             }
         }
 
@@ -54,34 +70,75 @@ namespace ServerNetworkController
         /// </summary>
         public void Stop()
         {
-            Listening = false;
+            Listening = false; // Stops the loop that is listening for clients.
 
             foreach (var client in Clients)
             {
-                ClientLeft?.Invoke(client.Key);
-                client.Value.Close();
+                ClientLeft?.Invoke(client.Key); // Send an event that this client has disconnected  
+                client.Value.Close(); // Now disconnect the client.
             }
 
-            TcpListener.Stop();
+            TcpListener.Stop(); // Stop the server.
         }
 
 
+        /// <summary>
+        /// Sends the specified string arrays to all of the clients.
+        /// </summary>
+        /// <param name="strs"></param>
+        public void SendStringsGlobal(string[] strs)
+        {
+            foreach (var client in Clients)
+                SendStrings(client.Key, strs);
+        }
+
+        /// <summary>
+        /// Sends the specified string to all clients.
+        /// </summary>
+        /// <param name="str">The string to send</param>
         public void SendStringGlobal(string str)
         {
-            foreach(var client in Clients)
+            foreach (var client in Clients)
                 SendString(client.Key, str);
         }
 
+        /// <summary>
+        /// Will send the specified string to the specified client. Will append string with new line.
+        /// </summary>
+        /// <param name="uid">The client to send the data to</param>
+        /// <param name="str">The string data to send</param>
         public void SendString(int uid, string str)
         {
             if (!Clients.ContainsKey(uid)) return;
 
             var stream = Clients[uid].GetStream();
 
-            var bytes = Encoding.UTF8.GetBytes(str);
+            var bytes = Encoding.UTF8.GetBytes(str + "\n");
 
             stream.Write(bytes, 0, bytes.Length);
         }
 
+        /// <summary>
+        /// Sends an array of strings to the specified client. Will append a newline to each entry
+        /// </summary>
+        /// <param name="uid">The UID to send it to</param>
+        /// <param name="strs">The array of strings</param>
+        public void SendStrings(int uid, string[] strs)
+        {
+            if (!Clients.ContainsKey(uid)) return;
+
+            var stream = Clients[uid].GetStream();
+
+            var memStream = new MemoryStream();
+
+            foreach (var str in strs)
+            {
+                var bytes = Encoding.UTF8.GetBytes(str + "\n");
+                memStream.Write(bytes, 0, bytes.Length);
+            }
+
+            var buffer = memStream.GetBuffer();
+            stream.Write(buffer, 0, buffer.Length);
+        }
     }
 }
